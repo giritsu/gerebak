e97d56f0b32258329c45a184da09e4fc6b1dd1bd
Muhammad Afrinza <64436973+giritsu@users.noreply.github.com>
2026-03-09 14:05:19 +0000

    update

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 8 ENC-0# >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 8 ENC-03 >/dev/null 2>&1 &
 sleep 30
 while true
 do
